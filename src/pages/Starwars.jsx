46b3a459da058aee07406f7eb57ae5fbd2eb7209
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-04-26 23:19:44 +0000

    add Starwaras.jsx and route

---
+++ src/pages/Starwars.jsx
@@ -0,0 +1,18 @@
+// Import necessary components from react-router-dom and other parts of the application.
+import { Link } from "react-router-dom";
+//import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+
+export const Starwars = () => {
+  // Access the global state and dispatch function using the useGlobalReducer hook.
+  //const { store, dispatch } = useGlobalReducer()
+
+  return (
+    <div className="container">
+      <h2>Starwars</h2>
+
+      <Link to="/">
+        <button className="btn btn-primary">Back home</button>
+      </Link>
+    </div>
+  );
+};
